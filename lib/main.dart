d4cb8ad7f7f65b300997e10855fc053b4639be7a
muckystack <MiguelMartinezDW@outlook.com>
2019-08-22 13:47:06 -0500

    Cargo el nuevo widget donde se creara toda la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
  
-import 'package:flutter_componentes_fh/src/pages/home_temp.dart';
+// import 'package:flutter_componentes_fh/src/pages/home_temp.dart';
+import 'package:flutter_componentes_fh/src/pages/home_page.dart';
 
 
 void main() => runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
