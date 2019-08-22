90eaf629669d760e2a15435188a9f52b3ec4812c
muckystack <MiguelMartinezDW@outlook.com>
2019-08-22 11:55:24 -0500

    Agrego el listView a mi aplicación

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -5,8 +5,25 @@ class HomePageTemp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('List title')
+          ),
+          Divider(),
+          ListTile(
+            title: Text('List title')
+          ),
+          Divider(),
+          ListTile(
+            title: Text('List title')
+          ),
+        ],
+      ),
     );
   }
 }
