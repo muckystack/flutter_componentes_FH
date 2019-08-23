93008746498b740e19ae715e63deaf95774a840d
muckystack <MiguelMartinezDW@outlook.com>
2019-08-22 21:49:33 -0500

    Creo la segunda targeta (la cual tiene imagen cargada de internet). Agrego una imagen que se cargara mientras se carga otra imagen.

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -12,7 +12,9 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2()
         ],
       ),
     );
@@ -40,6 +42,29 @@ class CardPage extends StatelessWidget {
                 onPressed: () {},
               )
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage('https://i.ytimg.com/vi/BfCwN4iy6T8/maxresdefault.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          // Image(
+          //   image: NetworkImage(url),
+          // ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('No tengo idea de que poner')
           )
         ],
       ),
