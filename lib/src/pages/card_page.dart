76468a13d42ca54805629c59f30bfa67c8234471
muckystack <MiguelMartinezDW@outlook.com>
2019-08-22 20:23:07 -0500

    Creo la página de targetas y creo la primera targeta

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  const CardPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
+    );
+  }
+
+  // Targeta 1
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue,),
+            title: Text('Soy el titulo de esta targeta'),
+            subtitle: Text('Aqui estamos con la descripción que quiero que ustedes se hagan una idea de lo que quiero mostrar'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_componentes_fh/src/pages/alert_page.dart';
 import 'package:flutter_componentes_fh/src/pages/avatar_page.dart';
+import 'package:flutter_componentes_fh/src/pages/card_page.dart';
 import 'package:flutter_componentes_fh/src/pages/home_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -9,6 +10,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
+    'card': (BuildContext context) => CardPage(),
   };
 
 }
