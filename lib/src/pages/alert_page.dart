349ffba4bba91c0eba2ba4e22c8470c03ac559fe
muckystack <MiguelMartinezDW@outlook.com>
2019-08-23 10:19:55 -0500

    Creo cuadro de dialogo de la alerta

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,12 +8,61 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.location_city),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  // Función para mostrar alertas
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      // Esta instrucción permite o no(true, false) que se cierre la ventana al hacer click afuera del dialogo
+      barrierDismissible: true,
+      // Contrulle el dialog
+      builder: (context) {
+
+        return AlertDialog(
+          // Permite redondear el cuadro de dialogo de la alerta
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            // Esta instrucción ajusta la columna para que se adapte al tamaño minimo requerido para que se muestre el contenido
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este el el contenido de la caja de la alerta'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+          // Controles (Botones)
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+
+      }
     );
   }
 }
