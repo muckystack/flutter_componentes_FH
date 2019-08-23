f81c18b9b5b38a7e918058172834a9f7df90ebc2
muckystack <MiguelMartinezDW@outlook.com>
2019-08-23 10:50:12 -0500

    Creo una nueva pantalla en la cual se van a incluir animaciones en un container. Creo su ruta y cargo su incono para que se muestre en pantalla. Tambien cambio los estados con un boton flotante (falta la animacion)

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_componentes_fh/src/pages/alert_page.dart';
+import 'package:flutter_componentes_fh/src/pages/animated_container.dart';
 import 'package:flutter_componentes_fh/src/pages/avatar_page.dart';
 import 'package:flutter_componentes_fh/src/pages/card_page.dart';
 import 'package:flutter_componentes_fh/src/pages/home_page.dart';
@@ -11,6 +12,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
   };
 
 }
