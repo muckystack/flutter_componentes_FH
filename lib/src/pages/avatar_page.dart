9a9660d15752fd568f85c93cb6f1daf55a0df0d0
muckystack <MiguelMartinezDW@outlook.com>
2019-08-23 10:33:36 -0500

    Creo las imagenes de avatar en la parte del AppBar y tambien agrego otra imagen en el body con efecto de carga

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,34 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        // Nos permite poner widget en el appbar
+        actions: <Widget>[
+          Container(
+            // Afecta el padding del contenedor
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('http://mouse.latercera.com/wp-content/uploads/2019/03/lee.jpg'),
+              // Expresa el tamaño del avatar
+              radius: 21.0,
+            ),
+          ),
+          Container(
+            // Permite modificar el margen de un lado del contenedor
+            margin: EdgeInsets.only(right: 10.0),
+            // Permite poner un avatar en el AppBar
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.purple
+            ),
+          )
+        ],
+      ),
+
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://ichef.bbci.co.uk/news/660/cpsprodpb/A014/production/_104308904_gettyimages-618580352.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        ),
       ),
     );
   }
